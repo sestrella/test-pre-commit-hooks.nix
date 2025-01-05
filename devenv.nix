1289c77ab3db377eee3164cc336b30f6eb1225a0
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-01-04 23:26:56 -0500

    Check terraform-format default

--- devenv.nix
+++ devenv.nix
@@ -7,6 +7,5 @@
   # languages.opentofu.enable = true;
 
   pre-commit.hooks.terraform-format.enable = true;
-  pre-commit.hooks.terraform-format.package = config.languages.terraform.package;
-  # pre-commit.hooks.terraform-format.package = config.languages.opentofu.package;
+  # pre-commit.hooks.terraform-format.package = config.languages.terraform.package;
 }
